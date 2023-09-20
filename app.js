73befe9c39f11b8e23e4569872fbfa6df15fd43a
rae charles <r_charles87@yahoo.com>
2023-09-20 19:42:22 -0400

    small test route to make sure it works

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const usersController = require('./controllers/usersController');
+const express = require("express");
+const cors = require("cors");
+const usersController = require("./controllers/usersController");
 
 const app = express();
 app.use(cors());
+const db = require("./db");
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ data: "Service is running "});
+app.get("/", (req, res) => {
+  res.status(200).json({ data: "Service is running " });
 });
 
-app.use('/users', usersController);
+app.use("/users", usersController);
 
-app.all('*', (req, res) => {
-  res.status(404).json({ error: "Not found"});
-})
+app.all("*", (req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
+
+app.get("/tests", async (request, response) => {
+  try {
+    const tests = await db.any("SELECT * FROM tests;");
+
+    response.status(200).json({ data: tests });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = app;
