570bad9df33fe8dbce057732b407ed570f26dbee
Bo Latt <bolattny@gmail.com>
2023-09-20 20:30:58 -0400

    refactor users query

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,7 +1,9 @@
-const pgp = require("pg-promise");
+const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const connection = { connectionString: process.env.DATABASE_URL };
+const connection = {
+  connectionString: process.env.DATABASE_URL,
+};
 
 const db = pgp(connection);
 
