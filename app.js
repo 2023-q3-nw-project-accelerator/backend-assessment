2a24f9af9cb7f2384a72f243cfb860b95858a63e
Wwright91 <wisdomwright-martin@pursuit.org>
2023-09-20 21:57:18 -0400

    Serve data from database, not json
    1. Updated queries to serve data from users table, not json
    2. Updated controllers to await asynchronous query functions.
    3. Deleted usersData.json
    4. Deleted /tests route

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./db");
 const usersController = require("./controllers/usersController");
 
 const app = express();
@@ -9,17 +8,6 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({ data: "Service is running " });
-});
-
-// Remove This!!!!
-app.get("/tests", async (request, response) => {
-  try {
-    const tests = await db.any("SELECT * FROM users");
-
-    response.status(200).json({ data: tests });
-  } catch (err) {
-    response.status(500).json({ error: err.message });
-  }
 });
 
 app.use("/users", usersController);
