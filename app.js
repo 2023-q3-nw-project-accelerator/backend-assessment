cc9c7b104b01f3d41210bcd52d6bffece880b8e4
Wwright91 <wisdomwright-martin@pursuit.org>
2023-09-20 21:15:38 -0400

    Index.js And Seed_users.sql File Created
    1. Users seeded into users table.
    2. /tests route created to ensure connection between
    the API and external db.

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const usersController = require('./controllers/usersController');
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+const usersController = require("./controllers/usersController");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).json({ data: "Service is running "});
+app.get("/", (req, res) => {
+  res.status(200).json({ data: "Service is running " });
 });
 
-app.use('/users', usersController);
+app.get("/tests", async (request, response) => {
+  try {
+    const tests = await db.any("SELECT * FROM users");
 
-app.all('*', (req, res) => {
-  res.status(404).json({ error: "Not found"});
-})
+    response.status(200).json({ data: tests });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
+});
+
+app.use("/users", usersController);
+
+app.all("*", (req, res) => {
+  res.status(404).json({ error: "Not found" });
+});
 
 module.exports = app;
