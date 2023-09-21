6441573bd55b79d776f449eab8210cc0b83dd2ce
rae charles <r_charles87@yahoo.com>
2023-09-20 20:49:45 -0400

    Updated controllers and queries to use new api end point

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const { getAllUsers, getUserById } = require('../queries/usersQueries');
+const express = require("express");
+const { getAllUsers, getUserById } = require("../queries/usersQueries");
 
 const usersController = express.Router();
 
-usersController.get('/', (req, res) => {
-  users = getAllUsers() || [];
-  res.status(200).json({ data: users });
+usersController.get("/", async (req, res) => {
+  try {
+    const users = await getAllUsers();
+    response.status(200).json({ data: users });
+  } catch (err) {
+    response.status(500).json({ error: err.message });
+  }
 });
 
-usersController.get('/:id', (req, res) => {
-  const { id } = req.params;
-  user = getUserById(id);
-  if (user) {
-    res.status(200).json({ data: user });
-  } else {
-    res.status(404).json({ error: `User with id ${id} not found`});
+usersController.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await getUserById(id);
+    if (user) {
+      res.status(200).json({ data: user });
+    }
+    res.status(404).json({ error: `User with id ${id} not found` });
+  } catch (error) {
+    res.status(500).json({ error: err.message });
   }
-})
+});
 
 module.exports = usersController;
