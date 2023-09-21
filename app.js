acea9920e157e41568b9ffd2164d939794486aa5
rae charles <r_charles87@yahoo.com>
2023-09-20 20:30:57 -0400

    added schema and seed files

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const usersController = require("./controllers/usersController");
 
 const app = express();
 app.use(cors());
-const db = require("./db");
 
 app.use(express.json());
 
@@ -18,14 +17,5 @@ app.all("*", (req, res) => {
   res.status(404).json({ error: "Not found" });
 });
 
-app.get("/tests", async (request, response) => {
-  try {
-    const tests = await db.any("SELECT * FROM tests;");
-
-    response.status(200).json({ data: tests });
-  } catch (err) {
-    response.status(500).json({ error: err.message });
-  }
-});
 
 module.exports = app;
