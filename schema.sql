d1b111f5a972e4e9d6a3bedc22933ee7ace646b6
Daniel Solaque <danielsolaque@pursuit.org>
2023-09-20 21:11:13 -0400

    create schema for users table

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users(
+    id SERIAL,
+    age INTEGER,
+    name VARCHAR,
+    company VARCHAR,
+    country VARCHAR,
+    photo VARCHAR,
+    about VARCHAR,
+    latitude FLOAT(6),
+    longitude FLOAT(6),
+);
+
+
