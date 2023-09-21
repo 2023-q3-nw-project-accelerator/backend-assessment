0239bfd27de24e5d06e36760caf446b4119bfd7a
Lennie Nurse <lnursej@gmail.com>
2023-09-20 20:13:38 -0400

    added db folder, schema and users table

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    age INT,
+    name VARCHAR(255),
+    company VARCHAR(255),
+    country VARCHAR(255),
+    photo VARCHAR(255),
+    about TEXT,
+    latitude DECIMAL(9, 6),
+    longitude DECIMAL(9, 6)
+);
