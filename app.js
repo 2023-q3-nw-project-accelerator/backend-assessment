06ab9de8903ad871edf9fa9752e4d77a45096bc7
Tech-Kat <katpelle@pursuit.org>
2023-09-21 11:10:50 -0400

    Additional changes app.js - test routes removed seed.sql data updated with the Additional data from the usersdata.json

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./components/db");
 const usersController = require("./components/controllers/usersController");
 
 const app = express();
@@ -9,16 +8,6 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({ data: "Service is running " });
-});
-
-app.get("/users", async (request, response) => {
-  try {
-    const users = await db.any("SELECT * FROM users");
-
-    response.status(200).json({ data: users });
-  } catch (err) {
-    response.status(500).json({ error: err.message });
-  }
 });
 
 app.use("/users", usersController);
